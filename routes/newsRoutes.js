2e4ba46a8222f48f8f9d7d06622e506c42c2f853
fruktoz <bacsajozesff@gmail.com>
2025-08-18 12:39:50 +0200

    news képméret limit módosítás

--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
 });
 
 const fileFilter = (req, file, cb) => {
-    const allowedTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/webp'];
+    const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
     if (allowedTypes.includes(file.mimetype)) cb(null, true);
     else cb(new Error('Csak képek engedélyezettek!'), false);
 };
@@ -24,7 +24,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
     storage,
     fileFilter,
-    limits: { fileSize: 4 * 1024 * 1024 }
+    limits: { fileSize: 6 * 1024 * 1024 }
 }).single('image');
 
 //  Új hír létrehozása 
