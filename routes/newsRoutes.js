2dcc1b961a988976c49759708eca8f029b1fdb06
fruktoz <bacsajozesff@gmail.com>
2025-08-18 13:00:11 +0200

    fix

--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -7,8 +7,15 @@ const authenticateToken = require('../middleware/authMiddleware');
 const { institutionNews, institutions, users } = require('../dbHandler');
 
 // Multer config
+const uploadDir = path.join(__dirname, '..', 'uploads', 'news');
+
+// ha nincs meg a könyvtár, hozzuk létre
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, path.join('uploads', 'news')),
+    destination: (req, file, cb) => cb(null, uploadDir),
     filename: (req, file, cb) => {
         const ext = path.extname(file.originalname);
         cb(null, Date.now() + ext);
