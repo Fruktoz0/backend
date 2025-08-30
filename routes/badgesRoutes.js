71309aa10ebd7c6dac6aed1b2768d8841b57e0cc
fruktoz <bacsajozesff@gmail.com>
2025-08-30 12:34:20 +0200

    fix

--- routes/badgesRoutes.js
+++ routes/badgesRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const authenticateToken = require('../middleware/authMiddleware');
 
-router.get()
+
 
 
 module.exports = router;
