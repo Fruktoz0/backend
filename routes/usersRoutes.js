c170a16c20663cafb5e4b12744582eb3c4ecc09f
fruktoz <bacsajozesff@gmail.com>
2025-09-14 00:55:31 +0200

    fix

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -12,8 +12,10 @@ const { Op } = require('sequelize');
 const serviceAccount = {
     type: "service_account",
     project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
     private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
 };
 
 // 2. Firebase Admin SDK inicializálása
