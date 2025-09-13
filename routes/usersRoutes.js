0fa1742c3bf8b9a5e3c7a402c766c6c784853660
fruktoz <bacsajozesff@gmail.com>
2025-09-13 22:32:34 +0200

    firebase service account fix

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -9,7 +9,12 @@ const test_y = process.env.TEST_Y;
 const { Op } = require('sequelize');
 
 // 1. Service Account betöltése
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = {
+    type: "service_account",
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+};
 
 // 2. Firebase Admin SDK inicializálása
 if (!admin.apps.length) {
