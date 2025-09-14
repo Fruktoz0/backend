0e591ba019b4e649b0f20f36ec11f1b02b7dbd1e
fruktoz <bacsajozesff@gmail.com>
2025-09-14 12:07:25 +0200

    dbHandlerfix

--- dbHandler.js
+++ dbHandler.js
@@ -151,7 +151,7 @@ const reports = dbConnection.define('report', {
     },
     'status': {
         type: DataTypes.ENUM,
-        values: ['open', 'accepted', 'forwarded', 'in_progress', 'rejected', 'resolved', 'reopened' ]
+        values: ['open', 'accepted', 'forwarded', 'in_progress', 'rejected', 'resolved', 'reopened' ],
         defaultValue: 'open'
     },
     'institutionId': {
