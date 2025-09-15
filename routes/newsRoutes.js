47b71078aef347c425fad6f06bc80a79a34a2e8f
fecs1peti <fecso.peter23fi@egressy.info>
2025-09-15 11:26:45 +0200

    Update newsRoutes.js - db szám lekérdezés egyszerűsítése

--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -41,10 +41,10 @@ const upload = multer({
 router.post('/news_Inst_db',authenticateToken , async (req, res) => {
     try {
         const { institutionId } = req.body
-        const allNews = await institutionNews.findAll({
+        const a_db = await institutionNews.count({
             where: { institutionId }         
         })
-        res.status(200).json({ found_db: allNews.length });
+        res.status(200).json({ found_db: a_db });
     } catch (error) {
         console.error('Hiba a bejelentések lekérésekor:', error);
         res.status(500).json({ message: 'Szerverhiba a bejelentések lekérésekor' });
@@ -56,10 +56,10 @@ router.post('/news_Inst_db',authenticateToken , async (req, res) => {
 router.post('/report_Cat_db',authenticateToken , async (req, res) => {
     try {
         const { categoryId } = req.body
-        const allReports = await reports.findAll({
+        const a_db = await reports.count({
             where: { categoryId }         
         })
-        res.status(200).json({ found_db: allReports.length });
+        res.status(200).json({ found_db: a_db });
     } catch (error) {
         console.error('Hiba a bejelentések lekérésekor:', error);
         res.status(500).json({ message: 'Szerverhiba a bejelentések lekérésekor' });
